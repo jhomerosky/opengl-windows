37d56db12c68015bb0a56b564297344766816284
man <you@example.com>
2025-06-29 15:55:15 -0500

    textures working for one manual load

--- src/structs.hpp
+++ src/structs.hpp
@@ -6,16 +6,21 @@
 #define __MAX_MESHES__ 2048
 #define __MAX_MODELS__ 65536
 
+// vertex object is a collection of 3d point in space, 3d normal vector, and 2d texture coord
+// more than just a point in space, it is an object that encodes the corner of a polygon
 struct Vertex {
 	float pos[3];
 	float normal[3];
 	float texture[2];
 };
 
+// face encodes a triangle, which references 3 points in space as corners of the triangle
+// the meaning of the ID is left to the mesh which owns the face
 struct Face {
 	unsigned int vertexId[3];
 };
 
+// Mesh is a resource containing a list of vertices, a list of faces and some metadata
 struct Mesh {
 	Vertex* vertices = nullptr;
 	Face* faces = nullptr;
@@ -32,13 +37,21 @@ struct Mesh {
 	~Mesh() { free(vertices); free(faces); }
 };
 
+// Texture is a resource containing metadata
+struct Texture {
+	GLuint textureID;
+};
+
+// MeshInstance is a world model which references a mesh and a texture
+// TODO: store world data here and compute model on the fly
 struct MeshInstance {
 	Mesh* mesh;
-	char* texture;
+	Texture* texture;
 	glm::mat4 model;
 	glm::vec3 color;
 };
 
+// Camera encodes the view
 // yaw should be initialized to -90.0f
 struct Camera {
 	glm::vec3 pos;
@@ -56,6 +69,7 @@ struct Camera {
 	float TILT_SPEED;
 };
 
+// MouseInfo stores some state related to the mouse input
 // See initMouseInfo for default vals
 struct MouseInfo {
 	float lastX;
@@ -67,11 +81,13 @@ struct MouseInfo {
 	float modeSwitchCooldown;
 };
 
+// lightsource is an emitter of light for dynamic lighting
 struct LightSource {
 	glm::vec3 pos;
 	glm::vec3 color;
 };
 
+// Scene is meant to be a global scope singleton containing world state
 struct Scene {
 	MeshInstance* meshInstances[__MAX_MODELS__];
 	int meshInstanceCount;
@@ -81,9 +97,14 @@ struct Scene {
 	MouseInfo mouse;
 };
 
+// ResourcePool is meant to be a store of assets with IDs for MeshInstances to reference
 struct ResourcePool {
 	Mesh* meshes[__MAX_MESHES__];
 	int meshCount;
+	
+	// @TODO: add textures here? Or have 2 resourcePools?
+	// ResourcePool globalMeshPool;
+	// ResourcePool globalTexturePool;
 
 	// @TODO: map string name --> resourceID
 	//ResourceMap meshMap;
