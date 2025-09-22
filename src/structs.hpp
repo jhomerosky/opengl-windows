e8b11d4c2856672daa11d1a318d8248443ef4c01
man <35518194+jhomerosky@users.noreply.github.com>
2025-09-21 23:52:06 -0500

    some refactoring

--- src/structs.hpp
+++ src/structs.hpp
@@ -20,8 +20,8 @@ struct Face {
 
 // Mesh is a resource containing a list of vertices, a list of faces and some metadata
 struct Mesh {
-	Vertex* vertices = nullptr;
-	Face* faces = nullptr;
+	Vertex* vertices;
+	Face* faces;
 
 	size_t num_vertices;
 	size_t num_faces;
@@ -31,9 +31,21 @@ struct Mesh {
 	unsigned int VAO;
 	unsigned int VBO;
 	unsigned int EBO;
-
-	~Mesh() { free(vertices); free(faces); }
 };
+
+void init_mesh(Mesh* mesh) {
+	mesh->vertices = nullptr;
+	mesh->faces = nullptr;
+	mesh->num_vertices = 0;
+	mesh->num_faces = 0;
+	mesh->has_normals = false;
+}
+
+void free_mesh(Mesh* mesh) {
+	free(mesh->vertices);
+	free(mesh->faces);
+	free(mesh);
+}
 
 // Texture is a resource containing metadata
 struct Texture {
@@ -51,7 +63,7 @@ struct Skybox {
 
 // MeshInstance is a world model which references a mesh and a texture
 struct MeshInstance {
-	Mesh* mesh;
+	Mesh* mesh; // todo: make this a ptr to mesh's ID with global pool?
 	Texture* texture;
 	float pos[3];
 	float scale[3];
@@ -102,13 +114,13 @@ struct Scene {
 	LightSource lightSource;
 	Skybox skybox;
 	MouseInfo mouse;
-
-	~Scene() {
-		for (int i = 0; i < meshInstanceCount; i++) {
-			free(meshInstances[i]);
-		}
-	}
 };
+
+void free_scene(Scene* scene) {
+	for (int i = 0; i < scene->meshInstanceCount; i++) {
+		free(scene->meshInstances[i]);
+	}
+}
 
 // ResourcePool is meant to be a store of assets with IDs for MeshInstances to reference
 struct ResourcePool {
@@ -128,17 +140,13 @@ struct ResourcePool {
 	// Why should this pool own a map to its resources?
 	// The index is already an ID for the array. 
 	//ResourceMap meshMap;
-
-	~ResourcePool() {
-		for (int i = 0; i < meshCount; i++) {
-			free(meshes[i]);
-		}
-
-		for (int i = 0; i < textureCount; i++) {
-			free(textures[i]);
-		}
-	}
 };
+
+void free_resource_pool(ResourcePool* pool) {
+	for (int i = 0; i < pool->meshCount; i++) {
+		free_mesh(pool->meshes[i]);
+	}
+}
 
 #define __my_structs__
 #endif
